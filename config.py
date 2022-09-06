9a3146a4b0159075487af16cd429c2ddc1a669af
junos7 <66702724+junos7@users.noreply.github.com>
2022-09-06 14:03:50 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import base64
 
-userSite = '05107472990'
+userSite = ''
 
 senhaCod = '' #em base64
 senhaSQL = '' #em base64
@@ -8,9 +8,9 @@ senhaSQL = '' #em base64
 senhaSite = base64.b64decode(senhaCod).decode('utf-8')
 senhaSQL = base64.b64decode(senhaSQL).decode('utf-8')
 
-siteLogin = 'https://www.prppg.ufpr.br/siga/login'
-siteAcesso = 'https://www.prppg.ufpr.br/siga/selecionanivelacesso?comboAcesso=TXSEPX40001016070G0XSEPX47XSEPX6XSEPXXSEPX0XSEPX1'
-siteDados = f'https://www.prppg.ufpr.br/siga/graduacao/discente?d={id}&aba=informacoes'
+siteLogin = ''
+siteAcesso = ''
+siteDados = f''
 
 idInicial = 39437
 idFinal = 85432
